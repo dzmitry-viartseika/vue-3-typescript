0c2ef53eb2bb9a0ecbde8c55951626212037bb03
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-16 17:01:55 +0200

    removed default components and files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/todos">TODOS</router-link>
+    <router-link to="/todos">Todos</router-link>
   </nav>
   <router-view/>
 </template>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: [],
   },
   getters: {
+    todos(state) {
+      return state.todos;
+    }
   },
   mutations: {
   },
